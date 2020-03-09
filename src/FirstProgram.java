75193e4066cdfeebfbc2ce553588e5ee136f4a5e
Sintija-Git <sintija.jaspere@gmail.com>
2020-03-09 19:40:32 +0200

    Run another test

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,7 +1,10 @@
 
 public class FirstProgram {
     public static void main(String args []) {
+    	
         System.out.println("Hello Girls!" );
+        System.out.println ("Bye");
+        System.out.println ("For test purposes");
 }//end main
 }
 
